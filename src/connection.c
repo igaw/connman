64c88f32f4ece5283463106e0e9446c57159da81
Daniel Wagner <wagi@monom.org>
2020-11-29 12:16:53 +0100

    connection: Delete IPv6 default routes
    The correct way to delete the default route is to use
    __connman_inet_del_default_from_table. connman_inet_clear_ipv6_gateway_address()
    will only remove the additional route to reach the gateway on given
    interface but not the default route. IPv4 and IPv6 seems to behave
    slightly different, as connman_inet_clear_ipv4_gateway_address removes
    the default route as well.

--- src/connection.c
+++ src/connection.c
@@ -579,12 +579,12 @@ static void unset_default_gateway(struct gateway_data *data,
 	}
 
 	if (do_ipv4 && data->ipv4_gateway)
-		connman_inet_clear_gateway_address(index,
-						data->ipv4_gateway->gateway);
+		__connman_inet_del_default_from_table(RT_TABLE_MAIN,
+					index, data->ipv4_gateway->gateway);
 
 	if (do_ipv6 && data->ipv6_gateway)
-		connman_inet_clear_gateway_address(index,
-                                                data->ipv6_gateway->gateway);
+		__connman_inet_del_default_from_table(RT_TABLE_MAIN,
+					index, data->ipv6_gateway->gateway);
 }
 
 static struct gateway_data *find_default_gateway(void)
